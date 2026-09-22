8070fadcf1fb99f1d1e0623e0a249a960337d23e
derrick.musugi@student.vinci.be <derrick.musugi@student.vinci.be>
2026-09-22 18:55:58 +0200

    route de Derrick 2nd test

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,17 @@ const port = 3000;
 
 app.use(express.json());
 
+app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
+});
+
+
+
+
+
 app.get('/derrick', (_req, res) => {
   res.send('Route de derrick !');
 });
 
-app.listen(port, () => {
-  console.log(`Serveur lancé sur http://localhost:${port}`);
-});
 
 export default app;
